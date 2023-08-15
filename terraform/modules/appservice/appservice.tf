c2475d82bef963268674c993fd1f19f90e3076aa
Deepak Arora <talkdeepak@gmail.com>
2023-08-15 11:19:45 +0200

    terrafrom aap service plan

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -10,7 +10,7 @@ resource "azurerm_windows_web_app" "az_Windows_WebApp" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = var.location
   resource_group_name = var.resource_group
-  service_plan_id     = azurerm_service_plan.test.id
+  service_plan_id     = azurerm_service_plan.az_serv_plan.id
 
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE"     = 0
