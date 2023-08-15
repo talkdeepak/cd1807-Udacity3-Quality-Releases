e1c72b10ce4cfe18da9386ee770a21dde55eefaa
Deepak Arora <talkdeepak@gmail.com>
2023-08-15 11:11:58 +0200

    trrafrom appservice changes

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -1,21 +1,26 @@
-resource "azurerm_service_plan" "test" {
+resource "azurerm_service_plan" "az_serv_plan" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = var.location
   resource_group_name = var.resource_group
-  os_type             = "Linux"
+  os_type             = "Windows"
   sku_name            = "F1"
 }
 
-resource "azurerm_linux_web_app" "test" {
+resource "azurerm_windows_web_app" "az_Windows_WebApp" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = var.location
   resource_group_name = var.resource_group
   service_plan_id     = azurerm_service_plan.test.id
 
   app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE" = 1
+    "WEBSITE_RUN_FROM_PACKAGE"     = 0
+    SCM_DO_BUILD_DURING_DEPLOYMENT = true
   }
   site_config {
-    always_on = false
+    always_on                = false
+    remote_debugging_enabled = true
+    application_stack {
+      dotnet_version = "v6.0"
+    }
   }
 }
